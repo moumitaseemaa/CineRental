c1bc2b756e55fd1728684c4fd80857254d891f66
moumitaseemaa <moumitaseemaa@gmail.com>
2025-07-22 18:26:44 +0600

    footer has been changed

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="py-6 md:py-8 mt-8">
 		<div className="container mx-auto">
 			<p className="text-center text-sm text-black/30 dark:text-[#EEEEEE] ">
-				Copyright ©2024 | All rights reserved by Learn with Sumit
+				Copyright ©2025 | All rights reserved by Seema
 			</p>
 		</div>
 	</footer>
